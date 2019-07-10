ed24adcd50f2f71fd1aba70b61370d7db0c67b75
Екатерина Савина <esavina@bkcco.ru>
2019-07-10 17:57:46 +0300

    added media

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@ $(".switch__link").click(function(e) {
     e.preventDefault();
 
     var toggleText = $(this).data('toggle-text');
-
     $(this).data('toggle-text', $(this).text()).text(toggleText);
 
     $(".tabs__week").toggleClass( "active" );
     $(".week").toggleClass( "active" );
+});
+
+$(function(){
+    $("#phone").mask("+7-999-999-99-99");
+    $("#phone2").mask("+7-999-999-99-99");
+    $("#phone3").mask("+7-999-999-99-99");
+    $("#phone4").mask("+7-999-999-99-99");
 });
