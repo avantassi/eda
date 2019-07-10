30f327e5e4b11c1d050b3699b421eac4d4f4935e
Екатерина Савина <esavina@bkcco.ru>
2019-07-10 16:03:41 +0300

    fixed tabs bug

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(".switch__link").click(function(e) {
     e.preventDefault();
+
+    var toggleText = $(this).data('toggle-text');
+
+    $(this).data('toggle-text', $(this).text()).text(toggleText);
+
     $(".tabs__week").toggleClass( "active" );
     $(".week").toggleClass( "active" );
 });
